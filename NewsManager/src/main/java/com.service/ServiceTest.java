ff90644abc6e15c5203dcbcf40556a398774a614
zp <m18279651236@163.com>
2017-02-25 10:38:07 +0800

    update web

---
+++ NewsManager/src/main/java/com.service/ServiceTest.java
@@ -0,0 +1,11 @@
+package com.service;
+
+/**
+ * Created by Administrator on 2017/2/25 0025.
+ */
+public class ServiceTest {
+    public static void main(String[] args) {
+        System.out.println("this is 啊");
+    }
+
+}
