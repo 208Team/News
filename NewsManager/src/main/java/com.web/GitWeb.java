8617c0aabb39970d4434229cf513cb5332201742
zp <m18279651236@163.com>
2017-02-25 10:17:49 +0800

    update web

---
+++ NewsManager/src/main/java/com.web/GitWeb.java
@@ -0,0 +1,11 @@
+package com.web;
+
+/**
+ * Created by Administrator on 2017/2/25 0025.
+ */
+public class GitWeb {
+
+    public static void main(String[] args) {
+        System.out.println("web");
+    }
+}
