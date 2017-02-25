d2f1269a954cf37879266bef034cb61101533bf8
zp <m18279651236@163.com>
2017-02-25 11:05:07 +0800

    update hello

---
+++ NewsManager/src/main/java/com.dao/Hello.java
@@ -0,0 +1,11 @@
+package com.dao;
+
+/**
+ * Created by Administrator on 2017/2/25 0025.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("this is a hello");
+    }
+}
