58ef80dff1ed77487f77101989b187c9491b58ea
zp <m18279651236@163.com>
2017-02-25 10:17:31 +0800

    update service

---
+++ NewsManager/src/main/java/com.service/GitService.java
@@ -0,0 +1,12 @@
+package com.service;
+
+/**
+ * Created by Administrator on 2017/2/25 0025.
+ */
+public class GitService {
+
+    public static void main(String[] args) {
+        System.out.printf("service");
+
+    }
+}
