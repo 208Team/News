764a1d5ec5e01e3e2441c0415a8413a3ab05374d
zp <m18279651236@163.com>
2017-02-25 10:32:14 +0800

    update web

---
+++ NewsManager/src/main/java/com/entity/EntityTest.java
@@ -0,0 +1,11 @@
+package com.entity;
+
+/**
+ * Created by Administrator on 2017/2/25 0025.
+ */
+public class EntityTest {
+    public static void main(String[] args) {
+        System.out.println("hello this is a test");
+    }
+
+}
